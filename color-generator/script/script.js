27b74809c5efecde8934479e3b396a7eef584eea
StanKzchnko <49488521+StanislavKozachenko@users.noreply.github.com>
2021-04-18 17:16:20 +0300

    Add files via upload

---
+++ color-generator/script/script.js
@@ -0,0 +1,43 @@
+for(let i = 1; i <= 48; i++) {
+	const box = document.createElement('div');
+	box.classList.add('box');
+	document.querySelector(".container").appendChild(box);
+}
+const btn = document.querySelector('.brn');
+const randomColorBlock = document.querySelectorAll('.box');
+
+function RandomHexColorCode(){
+	var chars = '0123456789abcdef';
+	var colorLength = 6;
+	var color = "";
+
+	for(var i = 0; i < colorLength; i++) {
+		var randomColor = Math.floor(Math.random() * chars.length);
+		color += chars.substring(randomColor, randomColor + 1);
+	}
+	return "#"+color;
+}
+function copytext(el) {
+    var $tmp = $(".box");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    document.execCommand("copy");
+    $tmp.remove();
+}
+function addColor(){
+	randomColorBlock.forEach(e => {
+		var newColor = RandomHexColorCode();
+		e.style.backgroundColor = newColor;
+		e.innerHTML = newColor;
+	})
+}
+jQuery(document).ready(function($){
+		$('.box').click(function() {
+		var $text_copy = $(this);
+		var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val($text_copy.text()).select();
+		document.execCommand("copy");
+		$temp.remove();
+	});
+});
