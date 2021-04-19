55b9fcb2b4896a4834cc7479bf41eab32600ccc6
StanKzchnko <49488521+StanislavKozachenko@users.noreply.github.com>
2021-04-19 20:15:31 +0300

    Update script.js

--- color-generator/script/script.js
+++ color-generator/script/script.js
@@ -1,16 +1,17 @@
 for(let i = 1; i <= 24; i++) {
 	const box = document.createElement('div');
+	const front_box = document.createElement('div');
+	const back_box = document.createElement('div');
 	box.classList.add('box');
+	box.innerHTML = '<div class="front_box"></div><div class="back_box"></div>';
 	document.querySelector(".container").appendChild(box);
 }
-const btn = document.querySelector('.brn');
 const randomColorBlock = document.querySelectorAll('.box');
 
 function RandomHexColorCode(){
 	var chars = '0123456789ABCDEF';
 	var colorLength = 6;
 	var color = "";
-
 	for(var i = 0; i < colorLength; i++) {
 		var randomColor = Math.floor(Math.random() * chars.length);
 		color += chars.substring(randomColor, randomColor + 1);
@@ -27,9 +28,11 @@ function copytext(el) {
 function addColor(){
 	randomColorBlock.forEach(e => {
 		var newColor = RandomHexColorCode();
-		e.style.backgroundColor = newColor;
-		e.innerHTML = newColor;
+		e.querySelector(".front_box").style.backgroundColor = newColor;
+		e.querySelector(".front_box").innerHTML = newColor;
+		e.querySelector(".back_box").style.backgroundColor = newColor;
 	});
+
 }
 function scrollSlow(id){
 		var offset = 0;
@@ -39,3 +42,13 @@ function scrollSlow(id){
 		return false;
 }
 addColor();
+jQuery(document).ready(function($){
+		$('.box').click(function() {
+		var $text_copy = $(this);
+		var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val($text_copy.text()).select();
+		document.execCommand("copy");
+		$temp.remove();
+	});
+});
