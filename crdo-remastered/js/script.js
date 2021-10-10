e755f4705cd98a13cfc6205cee7720c14909e167
StanKzchnko <49488521+StanislavKozachenko@users.noreply.github.com>
2021-10-10 15:37:26 +0300

    Update script.js

--- crdo-remastered/js/script.js
+++ crdo-remastered/js/script.js
@@ -42,42 +42,12 @@ function scrollSlow(id){
     }
 });*/
 $(document).ready(function(){
-    $('.mobile_table_name').click(function(){
-        $('.relation__drop > .mobile_table_block_drop', $(this).parent()).toggleClass('mobile_table_block_drop_open');
-        $('.mobile_alert', $(this).parent()).toggleClass('mobile_alert_open');
+    const $block = $('.news_content_blocks_element').first().clone();
+    $('.news_content_btn_2').click(function() {
+        $(".news_content_blocks").append($block.clone());
+        $(".news_content_blocks").append($block.clone());
+        $(".news_content_blocks").append($block.clone());
     });
-    $('.alert_burger').click(function(){
-        $('.burger_drop_menu', $(this).parent()).toggleClass("burger_drop_menu_off");
-        $('.burger_drop_menu', $(this).parent()).toggleClass("burger_drop_menu_static");
-        $('.alert_arrow', $(this).parent()).toggleClass("alert_arrow_active");
-    });
-    $('.nav_menu_burger').click(function(){
-        $('.alerts_burger').toggleClass("dsplayNn");
-        $('.nav_burger_menu_main').toggleClass("nav_burger_menu_main_active");
-        $('.nav_burger_menu_main').toggleClass('nav_burger_menu_main_open');
-        setTimeout(function(){$('.nav_menu_burger').delay().toggleClass("nav_menu_burger_active");},1)
-        $(".top_out_btn").addClass('disable');
-        $(".burger_btn").click(function(){
-           $('.nav_menu_burger').removeClass('disable');
-        });
-    });
-jQuery(function($){
-  $(document).mouseup(function (e){ // событие клика по веб-документу
-    var div = $(".user_data"); // тут указываем ID элемента
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-            $('.feedback_content_element_add_text', $(".user_data").parent()).css("opacity", "0");
-            $('.feedback_content_element_add_text', $(".user_data").parent()).css("transition", ".2s");
-            $(".user_data").removeClass('user_data_clicked');
-    }
-  });
-});
-const $block = $('.news_content_blocks_element').first().clone();
-$('.news_content_btn_2').click(function() {
-    $(".news_content_blocks").append($block.clone());
-    $(".news_content_blocks").append($block.clone());
-    $(".news_content_blocks").append($block.clone());
-});
 });
 const $block2 = $('.courses_examples_block_element').first().clone();
 const $block3 = $('.hr_courses').first().clone();
